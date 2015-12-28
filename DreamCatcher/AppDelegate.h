99a5fb96e075a67e77125b3b42021c62683bffcc
Brett Tau <brett.tau@gmail.com>
2015-12-28 16:02:31 -0600

    Initial Commit

---
+++ DreamCatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DreamCatcher
+//
+//  Created by Brett Tau on 12/28/15.
+//  Copyright (c) 2015 Brett Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DreamCatcher/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DreamCatcher
+//
+//  Created by Brett Tau on 12/28/15.
+//  Copyright (c) 2015 Brett Tau. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
